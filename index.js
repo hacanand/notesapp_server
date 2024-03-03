58f1ca2367f41393c1e5c691737fd8a101f992ec
anand <anandpromax@gmail.com>
2024-03-03 14:29:18 +0530

    BUG

--- index.js
+++ index.js
@@ -2,27 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 var cors = require("cors");
-const allowedOrigins = [
-  " https://notesapp-client.vercel.app/",
-  " https://notesapp-client.vercel.app",
-];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      if (allowedOrigins.includes(origin)) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    credentials: true,
+    
   })
-);
-
+)
 const connectDB = require("./src/config/database");
 const { PORT } = require("./src/config/dotenvConfig");
 const apiRoutes = require("./src/routes/index");
