fbe0d7e66deeec19328bb5705c91647e50c6fbf2
anand <anandpromax@gmail.com>
2024-03-03 14:03:33 +0530

    bug

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 var cors = require("cors");
-app.use(
-  cors({
-    origin: "https://notesapp-client.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+// app.use(
+//   cors({
+//     origin: "https://notesapp-client.vercel.app",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//   })
+// );
 
 const connectDB = require("./src/config/database");
 const { PORT } = require("./src/config/dotenvConfig");
